df9963e5886840795d0cc9344a8aeb7a8266ebfd
leasunhy <leasunhy@gmail.com>
2016-08-24 19:53:55 +0800

    NEW: add Permissions and TransactionAbortedException

---
+++ src/main/scala/Permissions.scala
@@ -0,0 +1,18 @@
+package simpledb;
+
+/** Class representing requested permissions to a relation/title.
+  * Private constructor with two static objects READ_ONLY and READ_WRITE that
+  * represent the two levels of permission.
+  */
+private class Permissions(val permLevel: Int) {
+  override def toString: String = permLevel match {
+    case 0 => "READ_ONLY"
+    case 1 => "READ_WRITE"
+    case _ => "UNKNOWN"
+  }
+}
+
+object Permissions {
+  val READ_ONLY  = new Permissions(0)
+  val READ_WRITE = new Permissions(1)
+}
---
+++ src/main/scala/TransactionAbortedException.scala
@@ -0,0 +1,8 @@
+package simpledb;
+
+class TransactionAbortedException() extends Exception {
+}
+
+object TransactionAbortedException {
+  private final val serialVersionUID = 1L;
+}
