90ea9e05680f0d552ce2e5279f475a13e1f49ae5
leasunhy <leasunhy@gmail.com>
2017-02-19 21:55:55 +0800

    ADD: ScanTest

--- src/test/scala/simpledb/systemtest/ScanTest.scala
+++ src/test/scala/simpledb/systemtest/ScanTest.scala
@@ -1,5 +1,10 @@
 package simpledb.systemtest
 
+import java.io.File
+
+import simpledb._
+
+import scala.collection.mutable.ArrayBuffer
 import scala.util.Random
 
 /**
@@ -11,9 +16,67 @@ class ScanTest extends SimpleDbTestBase {
   val r = new Random()
 
   /** Tests the scan operator for a table with the specified dimensions. */
-  def validateScan(columnSizes: Array[Int], rowSizes: Array[Int]) = {
+  private def validateScan(columnSizes: Array[Int], rowSizes: Array[Int]) = {
     for (columns <- columnSizes; rows <- rowSizes) {
-      val tuples = new
+      val tuples = ArrayBuffer.empty[ArrayBuffer[Int]]
+      val f = SystemTestUtil.createRandomHeapFile(columns, rows, null, tuples)
+      SystemTestUtil.matchTuples(f, tuples)
+      Database.resetBufferPool(BufferPool.DEFAULT_PAGES)
     }
+  }
+
+  /* Scan 1-4 columns. */
+  "The system" should "be able to scan small tables correctly." in {
+    val columnSizes = Vector(1, 2, 3, 4)
+    val rowSizes = Vector(0, 1, 2, 511, 512, 513, 1023, 1024, 1025, 4096 + r.nextInt(4096))
+  }
+
+  "A SeqScan iterator" should "work as expected." in {
+    val tuples = ArrayBuffer.empty[ArrayBuffer[Int]]
+    val f = SystemTestUtil.createRandomHeapFile(2, 1000, null, tuples)
+    val tid = new TransactionId()
+    val scan = new SeqScan(tid, f.getId, "table")
+    scan.open()
+    for (i <- 0 until 100) {
+      assert(scan.hasNext())
+      val t = scan.next()
+      assert(tuples(i) === SystemTestUtil.tupleToList(t))
+    }
+    scan.rewind()
+    for (i <- 0 until 100) {
+      assert(scan.hasNext())
+      val t = scan.next()
+      assert(tuples(i) === SystemTestUtil.tupleToList(t))
+    }
+    scan.close()
+    Database.getBufferPool.transactionComplete(tid)
+  }
+
+  "The buffer pool" should "actually cache data." in {
+    /* Counts the number of readPage operations. */
+    class InstrumentedHeapFile(f: File, td: TupleDesc) extends HeapFile(f, td) {
+      var readCount = 0
+      override def readPage(id: PageId): Page = {
+        readCount += 1
+        super.readPage(id)
+      }
+    }
+
+    // Create the table
+    val PAGES = 30
+    val tuples = ArrayBuffer.empty[ArrayBuffer[Int]]
+    val f = SystemTestUtil.createRandomHeapFileUnopened(1, 992 * PAGES, null, tuples, 1000)
+    val td = Utility.getTupleDesc(1)
+    val table = new InstrumentedHeapFile(f, td)
+    Database.getCatalog.addTable(table, SystemTestUtil.getUUID())
+
+    // Scan the table once
+    SystemTestUtil.matchTuples(table, tuples)
+    assert(PAGES === table.readCount)
+    table.readCount = 0
+
+    // Scan the table again: all pages should be cached
+    SystemTestUtil.matchTuples(table, tuples)
+    assert(0 === table.readCount)
   }
 }
