f61fbba33dde9e8a154f1d0fa2eef1b64c2f1ed0
leasunhy <leasunhy@gmail.com>
2017-02-20 11:37:14 +0800

    finish Catalog

--- src/main/scala/simpledb/Catalog.scala
+++ src/main/scala/simpledb/Catalog.scala
@@ -17,29 +17,38 @@ import scala.io.Source
   * @constructor Creates a new, empty catalog.
   */
 class Catalog {
+  private case class CatalogEntry(file: DbFile, name: String, primaryKey: String)
+
+  import scala.collection.mutable
+  private val tables = mutable.Map.empty[Int, CatalogEntry]
+  private val nameToId = mutable.Map.empty[String, Int]
+
   /**
     * Add a new table to the catalog.
     * This table's contents are stored in the specified DbFile.
     * @param file the contents of the table to add;  file.getId() is the identfier of
     *    this file/tupledesc param for the calls getTupleDesc and getFile
     * @param name the name of the table -- may be an empty string.  May not be null.  If a name
+    *    conflict exists, use the last table to be added as the table for a given name.
     * @param pkeyField the name of the primary key field
-    * conflict exists, use the last table to be added as the table for a given name.
     */
-  def addTable(file: DbFile, name: String, pkeyField: String = "") = ???
+  def addTable(file: DbFile, name: String, pkeyField: String = ""): Unit = {
+    nameToId(name) = file.getId
+    tables(file.getId) = CatalogEntry(file, name, pkeyField)
+  }
 
   /**
     * Return the id of the table with a specified name,
     * @throws NoSuchElementException if the table doesn't exist
     */
-  def getTableId(name: String): Int = ???
+  def getTableId(name: String): Int = tables(nameToId(name)).file.getId
 
   /**
     * Returns the tuple descriptor (schema) of the specified table
     * @param tableId The id of the table, as specified by the DbFile.getId()
     *     function passed to addTable
     */
-  def getTupleDesc(tableId: Int): TupleDesc = ???
+  def getTupleDesc(tableId: Int): TupleDesc = tables(tableId).file.getTupleDesc
 
   /**
     * Returns the DbFile that can be used to read the contents of the
@@ -47,23 +56,26 @@ class Catalog {
     * @param tableId The id of the table, as specified by the DbFile.getId()
     *     function passed to addTable
     */
-  def getDbFile(tableId: Int): DbFile = ???
+  def getDbFile(tableId: Int): DbFile = tables(tableId).file
 
   /** Delete all tables from the catalog. */
-  def clear(): Unit = ???
+  def clear(): Unit = {
+    nameToId.clear()
+    tables.clear()
+  }
 
   /** Get the primary key for table with id = tableId */
-  def getPrimaryKey(tableId: Int): String = ???
+  def getPrimaryKey(tableId: Int): String = tables(tableId).primaryKey
 
   /** Get the iterator of table ids. */
-  def tableIdIterator(): Iterator[Int] = ???
+  def tableIdIterator(): Iterator[Int] = tables.keysIterator
 
   /** Get the name of table with id = tableId. */
-  def getTableName(id: Int) = ???
+  def getTableName(id: Int): String = tables(id).name
 
   /**
     * Reads the schema from a file and creates the appropriate tables in the database.
-    * @param catalogFile
+    * @param catalogFile the path of the catalog file.
     */
   def loadSchema(catalogFile: String): Unit = {
     for (line <- Source.fromFile(catalogFile).getLines) {
